367e6ce8fc8e84867eb92be71fee145eb6934a8a
AnddyQG <anddysk819@gmail.com>
2024-05-29 19:34:15 -0500

    taller 13

--- js/app.js
+++ js/app.js
@@ -1,11 +1,46 @@
 console.log(Vue)
 
+const estudiantes=[{nombre:"Anddy",apellido:"Quisilema"},
+{nombre:"Filip-po",apellido:"Hasckiiv"},
+{nombre:"Pepito",apellido:"Perez"},
+{nombre:"Niño",apellido:"Moi"},
+{nombre:"Brus",apellido:"Banneado"}
+];
+
+
 const app = Vue.createApp({
-    template:`
-    <h1>Hola mundo desde VueJS</h1>
-    <p>{{1+1}}</p>
-    <p>{{[1, 2, 3]}}</p>
-    <p>{{true?'Verdadero':'Falso0'}}</p>
-    `
+  //  template:`
+   // <h1>Hola mundo desde VueJS</h1>
+    //<p>{{1+1}}</p>
+    //<p>{{[1, 2, 3]}}</p>
+    //<p>{{true?'Verdadero':'Falso0'}}</p>
+    //`
+  data(){
+    return{
+      mensaje: "Hola Mundo Propiedad Reactiva",
+      valor: 7,
+      lista: estudiantes
+    }
+  },
+  methods:{
+    
+    cambiarTexto(){
+      this.mensaje="Holitas de mar";
+      
+    },
+    sumar(){
+      this.valor=this.valor+10
+    },
+    agregar() {
+      const nuevo = {
+          nombre: "Anddy",
+          apellido: "Quisilema"
+      }
+      this.lista.unshift(nuevo);
+  }
+  
+  }
+
+
 });
 app.mount('#myApp')
